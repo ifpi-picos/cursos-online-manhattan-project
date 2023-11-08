c0301923c2a121e57554b5df939a5184dd47b1fa
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-11-07 23:39:03 -0300

    Ambiente pronto

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -1,11 +1,31 @@
 package br.edu.ifpi;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App {
+public class App extends Application{
+
+    private static Scene scene;
+
+     private Parent createContent() {
+        return new StackPane(new Text("Hello World"));
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sistema de cursos online!");
+        launch(args);
     }
 }
