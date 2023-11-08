378e5c1d3b0bcc4966b0b4dada2591a0d92f07ec
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-11-08 17:31:45 -0300

    add fonts e css

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -1,15 +1,9 @@
 package br.edu.ifpi;
 
-import java.io.File;
-import java.net.MalformedURLException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -18,12 +12,11 @@ import javafx.stage.Stage;
  */
 public class App extends Application{
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/telaLogIn.fxml"));
         Scene scene = new Scene(root);
+        // scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/br/edu/ifpi/controllers/ControladorTelaLogIn.java
+++ src/main/java/br/edu/ifpi/controllers/ControladorTelaLogIn.java
@@ -1,8 +1,11 @@
 package br.edu.ifpi.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class ControladorTelaLogIn {
     
@@ -14,6 +17,15 @@ public class ControladorTelaLogIn {
 
     @FXML
     private AnchorPane background;
+
+    @FXML
+    private PasswordField inputSenhaLogIn;
+
+    @FXML
+    private TextField inputUsuarioLogIn;
+
+    @FXML
+    private Text logInText;
 
 }
 
