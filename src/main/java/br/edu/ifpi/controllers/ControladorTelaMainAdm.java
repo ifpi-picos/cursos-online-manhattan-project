db4d2733b7f6ed0e92f7b1a99109698e8f2432e8
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-11-09 16:52:57 -0300

    feature- icons add telaLogin

--- src/main/java/br/edu/ifpi/controllers/ControladorTelaMainAdm.java
+++ src/main/java/br/edu/ifpi/controllers/ControladorTelaMainAdm.java
@@ -1,5 +1,10 @@
 package br.edu.ifpi.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class ControladorTelaMainAdm {
-    
+        @FXML
+    private AnchorPane Background;
+
 }
