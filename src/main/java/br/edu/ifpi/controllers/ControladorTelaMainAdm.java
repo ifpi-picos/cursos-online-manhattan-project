2ce41e1070b8c1336a90fb5df2b9ddfe4e9733bb
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-11-09 17:56:34 -0300

    feature - iniciando telaAdm

--- src/main/java/br/edu/ifpi/controllers/ControladorTelaMainAdm.java
+++ src/main/java/br/edu/ifpi/controllers/ControladorTelaMainAdm.java
@@ -1,10 +1,29 @@
 package br.edu.ifpi.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 public class ControladorTelaMainAdm {
-        @FXML
+           @FXML
     private AnchorPane Background;
+
+    @FXML
+    private Button alunosBotao;
+
+    @FXML
+    private Button configuracoesBotao;
+
+    @FXML
+    private Button cursosBotao;
+
+    @FXML
+    private Button homeBotao;
+
+    @FXML
+    private Button permissoesContasbotao;
+
+    @FXML
+    private Button professoresBotao;
 
 }
