1aaa63117162674bd8964e0a10d2da3abf1063c9
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 15:27:35 -0300

    Criando getters e setters

--- src/main/java/br/edu/ifpi/entidades/Professor.java
+++ src/main/java/br/edu/ifpi/entidades/Professor.java
@@ -4,25 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Professor {
-    private int id;
-    private String nome;
-    private String email;
-    private List<Curso> cursos; // Cursos que o professor ministra
+  private int id;
+  private String nome;
+  private String email;
+  private List<Curso> cursos; // Cursos que o professor ministra
 
-    // Construtores
-    
-    // Construtor sem id para facilitar a autenticação
-    public Professor(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-        this.cursos = new ArrayList<>();
-    }
-    
-    // Construtor completo para manipulação no sistema
-    public Professor(int id, String nome, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.cursos = new ArrayList<>();
-    }
+  // Construtores
+  
+  // Construtor sem id para facilitar a autenticação
+  public Professor(String nome, String email) {
+    this.nome = nome;
+    this.email = email;
+    this.cursos = new ArrayList<>();
+  }
+  
+  // Construtor completo para manipulação no sistema
+  public Professor(int id, String nome, String email) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.cursos = new ArrayList<>();
+  }
+  
+  // Getters e Setters
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public List<Curso> getCursos() {
+      return cursos;
+  }
+
+  public void setCursos(List<Curso> cursos) {
+      this.cursos = cursos;
+  }
 }
