61d8c74f2f029b54c281d417c4928661176bd105
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 18:15:26 -0300

    Método para consultar disciplinas

--- src/main/java/br/edu/ifpi/dao/DisciplinaDao.java
+++ src/main/java/br/edu/ifpi/dao/DisciplinaDao.java
@@ -16,4 +16,33 @@ public class DisciplinaDao implements Dao<Disciplina> {
     public DisciplinaDao(Connection connection) {
         this.connection = connection;
     }
+
+    @Override
+    public List<Disciplina> consultarTodos() {
+        List<Disciplina> disciplinas = new ArrayList<>();
+
+        String sql = "SELECT * FROM disciplinas";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                int cargaHoraria = resultSet.getInt("carga_horaria");
+                String ementa = resultSet.getString("ementa");
+                int professorId = resultSet.getInt("professor_id");
+
+                Professor professor = obterProfessorPorId(professorId);
+
+                Disciplina disciplina = new Disciplina(nome, cargaHoraria, ementa, professor);
+                disciplina.setId(id);
+                disciplinas.add(disciplina);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao consultar disciplinas", e);
+        }
+
+        return disciplinas;
+    }
 }
