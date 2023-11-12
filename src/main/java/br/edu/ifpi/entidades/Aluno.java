7b75141dcdecad4e142bebe538a96acc6f24f105
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 15:33:19 -0300

    Criando getters e setters

--- src/main/java/br/edu/ifpi/entidades/Aluno.java
+++ src/main/java/br/edu/ifpi/entidades/Aluno.java
@@ -25,4 +25,37 @@ public class Aluno {
         this.email = email;
         this.cursos = new ArrayList<>();
     }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
 }
