d3554c398b6d911f0389bff1441fb0a5408ab7cf
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 15:25:56 -0300

    Declarando atributos e os construtores

---
+++ src/main/java/br/edu/ifpi/entidades/Professor.java
@@ -0,0 +1,28 @@
+package br.edu.ifpi.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor {
+    private int id;
+    private String nome;
+    private String email;
+    private List<Curso> cursos; // Cursos que o professor ministra
+
+    // Construtores
+    
+    // Construtor sem id para facilitar a autenticação
+    public Professor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+        this.cursos = new ArrayList<>();
+    }
+    
+    // Construtor completo para manipulação no sistema
+    public Professor(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cursos = new ArrayList<>();
+    }
+}
