d215f32dde2eef9638e4d332a0fa8324acfda9c9
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 18:11:48 -0300

    Método para consultar alunos

--- src/main/java/br/edu/ifpi/dao/AlunoDao.java
+++ src/main/java/br/edu/ifpi/dao/AlunoDao.java
@@ -15,4 +15,28 @@ public class AlunoDao implements Dao<Aluno>{
     public AlunoDao(Connection connection) {
         this.connection = connection;
     }
+
+    @Override
+    public List<Aluno> consultarTodos() {
+        List<Aluno> alunos = new ArrayList<>();
+
+        String sql = "SELECT * FROM alunos";
+
+        try (Connection connection = Conexao.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                String email = resultSet.getString("email");
+
+                Aluno aluno = new Aluno(nome, id, email);
+                alunos.add(aluno);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao consultar alunos", e);
+        }
+        return alunos;
+    }
 }
