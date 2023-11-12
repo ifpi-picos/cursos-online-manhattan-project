266e391a62f169b6b564793545574c013fc54cf4
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-12 15:52:39 -0300

    Método para cadastrar um curso

--- src/main/java/br/edu/ifpi/dao/CursoDao.java
+++ src/main/java/br/edu/ifpi/dao/CursoDao.java
@@ -16,4 +16,33 @@ public class CursoDao implements Dao<Curso> {
     public CursoDao(Connection connection) {
         this.connection = connection;
     }
+
+    @Override
+    public int cadastrar(Curso curso) {
+        String sql = "INSERT INTO cursos (nome, status, carga_horaria, descricao, professor_id) VALUES (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, curso.getNome());
+            stmt.setBoolean(2, curso.getStatus());
+            stmt.setInt(3, curso.getCargaHoraria());
+            stmt.setString(4, curso.getDescricao());
+
+            // Certifique-se de que o professor não é nulo antes de acessar o ID
+            Professor professor = curso.getProfessor();
+            if (professor != null) {
+                stmt.setInt(5, professor.getId());
+            } else {
+                throw new RuntimeException("O professor associado ao curso é nulo");
+            }
+
+            stmt.execute();
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                curso.setId(rs.getInt(1));
+            }
+            return curso.getId();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
