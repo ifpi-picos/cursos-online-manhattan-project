ca1842f29c48ce68179c75459d010837672678e0
unknown <gustavofigueredo86@gmail.com>
2023-11-14 21:01:05 -0300

    Reparando cursodao

--- src/main/java/br/edu/ifpi/dao/CursoDao.java
+++ src/main/java/br/edu/ifpi/dao/CursoDao.java
@@ -1,8 +1,5 @@
 package br.edu.ifpi.dao;
 
-import br.edu.ifpi.entidades.Curso;
-import br.edu.ifpi.entidades.Professor;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,8 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.ifpi.entidades.Curso;
+import br.edu.ifpi.enums.StatusCurso;
+import br.edu.ifpi.entidades.Professor;
+
 public class CursoDao implements Dao<Curso> {
-    private final Connection connection;
+    private Connection connection;
 
     public CursoDao(Connection connection) {
         this.connection = connection;
@@ -19,89 +20,76 @@ public class CursoDao implements Dao<Curso> {
 
     @Override
     public int cadastrar(Curso curso) {
-        String sql = "INSERT INTO cursos (nome, status, carga_horaria, descricao, professor_id) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO curso (nome, descricao) VALUES (?, ?)";
 
-        try (PreparedStatement stmt = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, curso.getNome());
-            stmt.setBoolean(2, curso.getStatus());
-            stmt.setInt(3, curso.getCargaHoraria());
-            stmt.setString(4, curso.getDescricao());
-
-            // Certifique-se de que o professor não é nulo antes de acessar o ID
-            Professor professor = curso.getProfessor();
-            if (professor != null) {
-                stmt.setInt(5, professor.getId());
-            } else {
-                throw new RuntimeException("O professor associado ao curso é nulo");
-            }
+            stmt.setString(2, curso.getDescricao());
 
             stmt.execute();
             ResultSet rs = stmt.getGeneratedKeys();
             if (rs.next()) {
-                curso.setId(rs.getInt(1));
+                return rs.getInt(1);
             }
-            return curso.getId();
+            return -1;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+
     @Override
     public int remover(Curso curso) {
-        String sql = "DELETE FROM cursos WHERE id = ?";
+        String sql = "DELETE FROM curso WHERE id = ?";
 
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, curso.getId());
+            stmt.setLong(1, curso.getId());
+
             stmt.execute();
-            return curso.getId();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        return -1;
     }
 
     @Override
     public int alterar(Curso curso) {
-        String sql = "UPDATE cursos SET nome = ?, status = ?, carga_horaria = ?, descricao = ?, professor_id = ? WHERE id = ?";
+        String sql = "UPDATE curso SET nome = ?, descricao = ? WHERE id = ?";
 
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, curso.getNome());
-            stmt.setBoolean(2, curso.getStatus());
-            stmt.setInt(3, curso.getCargaHoraria());
-            stmt.setString(4, curso.getDescricao());
-            stmt.setInt(5, curso.getProfessor().getId());
-            stmt.setInt(6, curso.getId());
+            stmt.setString(2, curso.getDescricao());
+            stmt.setLong(3, curso.getId());
 
             stmt.execute();
-            return curso.getId();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return -1;
     }
 
     @Override
     public List<Curso> consultarTodos() {
-        String sql = "SELECT * FROM cursos";
+        String sql = "SELECT * FROM curso";
 
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
 
             List<Curso> cursos = new ArrayList<>();
 
-            // Cria uma instância de ProfessorDao usando a conexão existente
-            ProfessorDao professorDao = new ProfessorDao(connection);
-
             while (rs.next()) {
+                
                 String nome = rs.getString("nome");
-                boolean status = rs.getBoolean("status");
-                int cargaHoraria = rs.getInt("carga_horaria");
+                int id = rs.getInt("id");
+                String status = rs.getString("status");
+                int cargaHoraria =  rs.getInt("carga_horaria");
                 String descricao = rs.getString("descricao");
+                Professor professor = (new ProfessorDao(connection)).obterProfessorPorId(rs.getInt("professor_id"));
 
-                // Recupera o professor associado ao curso
-                int professorId = rs.getInt("professor_id");
-                Professor professor = professorDao.obterProfessorPorId(professorId);
+                Curso curso = new Curso(nome, null, cargaHoraria, descricao, professor);
+                curso.setId(id);
 
-                // Cria a instância de Curso com o professor recuperado
-                Curso curso = new Curso(nome, status, cargaHoraria, descricao, professor);
                 cursos.add(curso);
             }
 
@@ -110,4 +98,28 @@ public class CursoDao implements Dao<Curso> {
             throw new RuntimeException(e);
         }
     }
+
+    /* @Override
+    public Curso buscaPorId(Long id) {
+        String sql = "SELECT * FROM curso WHERE id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Curso curso = new Curso();
+                curso.setId(rs.getLong("id"));
+                curso.setNome(rs.getString("nome"));
+                curso.setDescricao(rs.getString("descricao"));
+
+                return curso;
+            }
+
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    } */
 }
