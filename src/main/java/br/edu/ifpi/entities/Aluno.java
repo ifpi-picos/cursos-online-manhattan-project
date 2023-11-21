260bf703023ea17f155bdcfc37498bfd5f5b48fd
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-20 21:15:20 -0300

    Declarando atributos e criando construtores

--- src/main/java/br/edu/ifpi/entities/Aluno.java
+++ src/main/java/br/edu/ifpi/entities/Aluno.java
@@ -1,5 +1,22 @@
 package br.edu.ifpi.entities;
 
 public class Aluno {
-    
+    private int id;
+    private String nome;
+    private String email;
+    private List<Curso> cursos; // Cursos que o aluno está matriculado
+
+    // Construtores
+    public Aluno(int id, String nome, String email, List<Curso> cursos) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cursos = cursos;
+    }
+
+    public Aluno(String nome, String email, List<Curso> cursos) {
+        this.nome = nome;
+        this.email = email;
+        this.cursos = cursos;
+    }
 }
