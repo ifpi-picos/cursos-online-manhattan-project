01e03a349bfff0555f03664c2f7a0ec036abd6d5
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-21 00:32:01 -0300

    integração do botão cadastrar-se

--- src/main/java/br/edu/ifpi/controllers/controladorLogin.java
+++ src/main/java/br/edu/ifpi/controllers/controladorLogin.java
@@ -3,6 +3,7 @@ package br.edu.ifpi.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import br.edu.ifpi.sistema;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -28,7 +29,14 @@ public class controladorLogin implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        btnCadastrar.setOnAction(event -> sistema.trocarCena("/fxml/cadastro.fxml", btnCadastrar));
 
+    }
+
+    public void autenticar(){
+        String nome = inputNome.getText();
+        String email = inputEmail.getText();
+        
 
     }
 
