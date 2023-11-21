af1293a665426b9a08d1ca9709fac6b331ba1199
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2023-11-21 20:43:00 -0300

    Lista AlunoCurso no Dao

--- src/main/java/br/edu/ifpi/dao/AlunoCurso.java
+++ src/main/java/br/edu/ifpi/dao/AlunoCurso.java
@@ -3,9 +3,13 @@ package br.edu.ifpi.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.ResultSet;
 
 import br.edu.ifpi.entities.Aluno;
 import br.edu.ifpi.entities.Curso;
+
 
 public class AlunoCurso{
     private Connection connection;
@@ -25,6 +29,35 @@ public class AlunoCurso{
             return stmt.executeUpdate();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao inserir Aluno_Curso no banco de dados: " + e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException("Erro ao fechar conexão com o banco de dados: " + e.getMessage());
+            }
+        }
+    }
+
+    public List<AlunoCurso> consultarTodos(Aluno aluno, Curso curso){
+        String sql = "SELECT * FROM Aluno_Curso WHERE id_aluno = ? AND id_curso = ?";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, aluno.getId());
+            stmt.setInt(2, curso.getId());
+            ResultSet rs = stmt.executeQuery();
+            List<AlunoCurso> alunoCurso = new ArrayList<>();
+
+            while (rs.next()) {
+                AlunoCurso ac = new AlunoCurso();
+                ac.setId(rs.getInt("id"));
+                ac.setIdAluno(rs.getInt("id_aluno"));
+                ac.setIdCurso(rs.getInt("id_curso"));
+                alunoCurso.add(ac);
+            }
+            return alunoCurso;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao consultar Aluno_Curso no banco de dados: " + e.getMessage());
         } finally {
             try {
                 connection.close();
