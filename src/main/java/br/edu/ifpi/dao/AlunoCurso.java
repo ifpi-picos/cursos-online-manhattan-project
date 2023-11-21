28234ef191e10bd852739cb94b83020c99b2158e
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-21 20:21:47 -0300

    Criando conexão e o construtor

--- src/main/java/br/edu/ifpi/dao/AlunoCurso.java
+++ src/main/java/br/edu/ifpi/dao/AlunoCurso.java
@@ -1,5 +1,11 @@
 package br.edu.ifpi.dao;
 
+import java.sql.Connection;
+
 public class AlunoCurso{
-    
+    private Connection connection;
+
+    public AlunoCurso(Connection connection) {
+        this.connection = connection;
+    }
 }
