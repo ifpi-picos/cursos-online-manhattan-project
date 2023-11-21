fc6217560b798ee2649021e58e0ab7555784aa83
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-21 06:34:50 -0300

    tela cadastro Curso implementado logica inicial

--- src/main/java/br/edu/ifpi/controllers/controladorGerenciarCursos.java
+++ src/main/java/br/edu/ifpi/controllers/controladorGerenciarCursos.java
@@ -1,11 +1,17 @@
 package br.edu.ifpi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import br.edu.ifpi.sistema;
 
-public class controladorGerenciarCursos {
+
+public class controladorGerenciarCursos implements Initializable {
 
     @FXML
     private Button btnAdicionar;
@@ -36,5 +42,15 @@ public class controladorGerenciarCursos {
 
     @FXML
     private TableView<?> tabelaCursos;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnCursos.setOnAction(event -> sistema.trocarCena("/fxml/gerenciarCursos.fxml",btnCursos));
+        btnHome.setOnAction(event -> sistema.trocarCena("/fxml/telaInicialProf.fxml", btnHome));
+        btnPerfil.setOnAction(event -> sistema.trocarCena("/fxml/perfilProfessor.fxml", btnPerfil));
+        btnSair.setOnAction(event -> sistema.trocarCena("/fxml/login.fxml", btnSair));
+
+        btnAdicionar.setOnAction(event -> sistema.trocarCena("/fxml/cadastroCurso.fxml", btnAdicionar));
+    }
 
 }
