01f9fc9908e1769385f64b7562c86c6802c33bec
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-21 03:52:29 -0300

    Função p/ carregar professores

--- src/main/java/br/edu/ifpi/controllers/controladorLogin.java
+++ src/main/java/br/edu/ifpi/controllers/controladorLogin.java
@@ -6,9 +6,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import br.edu.ifpi.sistema;
+import br.edu.ifpi.dao.Conexao;
+import br.edu.ifpi.dao.ProfessorDao;
+import br.edu.ifpi.entities.Professor;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -32,55 +37,38 @@ public class controladorLogin implements Initializable {
     @FXML
     private TextField inputNome;
 
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        List<Professor> professores = carregarProfessor();
         btnCadastrar.setOnAction(event -> sistema.trocarCena("/fxml/cadastro.fxml", btnCadastrar));
 
     }
 
-    public void autenticar(){
+    // Função para carregar uma lista de professores
+
+
+    public void autenticar(List<Professor> professores, List<Aluno> alunos){
         String nome = inputNome.getText();
         String email = inputEmail.getText();
     
-        // Substitua as informações do banco de dados conforme necessário
-        String url = "jdbc:postgresql://db.ijhnuifhmrhpcwvnlezj.supabase.co:5432/postgres?sslmode=require";
-        String usuarioBanco = "postgres";
-        String senhaBanco = "ifpi_bd2023";
-
-        // SQL para consultar se o aluno existe
-        String sql = "SELECT nome, email FROM alunos WHERE nome = ? AND email = ?";
-
-        try (
-            // Criação da conexão com o banco de dados
-            Connection conexao = DriverManager.getConnection(url, usuarioBanco, senhaBanco);
-            
-            // Preparação da consulta SQL
-            PreparedStatement stmt = conexao.prepareStatement(sql)
-        ) {
-            // Define os parâmetros da consulta
-            stmt.setString(1, nome);
-            stmt.setString(2, email);
-
-            // Executa a consulta
-            ResultSet resultado = stmt.executeQuery();
-
-            // Verifica se o aluno foi autenticado
-            if (resultado.next()) {
-                // Aluno autenticado
-                sistema.trocarCena("/fxml/telaInicial.fxml", btnEntrar);
-                String nomeAutenticado = resultado.getString("nome");
-                String emailAutenticado = resultado.getString("email");
-
-                System.out.println("Aluno autenticado:");
-                System.out.println("Nome: " + nomeAutenticado);
-                System.out.println("Email: " + emailAutenticado);
-            } else {
-                // Aluno não autenticado
-                System.out.println("Aluno não autenticado");
-            }
-
+        
+    }
+    
+    //Carregar professor
+    public List<Professor> carregarProfessor (){
+        Connection conexao = null;
+        try {
+            conexao = Conexao.getConnection();
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Trate a exceção conforme necessário
         }
-    }  
+
+        ProfessorDao professorDao = new ProfessorDao(conexao);
+
+        List<Professor> professores = professorDao.consultarTodos();
+
+        return professores;
+    }
+    
 }
