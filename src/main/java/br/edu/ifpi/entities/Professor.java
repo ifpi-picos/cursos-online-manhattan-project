d143fbd1be44b98c88eecf004d99741ad3dd5351
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2023-11-20 21:19:38 -0300

    constructores professor

--- src/main/java/br/edu/ifpi/entities/Professor.java
+++ src/main/java/br/edu/ifpi/entities/Professor.java
@@ -13,4 +13,10 @@ public class Professor {
         this.cursos = new ArrayList<>();
     }
     // Construtor completo para manipulação no sistema
+    public Professor(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cursos = new ArrayList<>();
+    }
 }
