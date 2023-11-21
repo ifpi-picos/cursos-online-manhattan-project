78e05997b1197ec16b46255e2360232736a110a3
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2023-11-21 02:32:32 -0300

    Conexao bd

--- src/main/java/br/edu/ifpi/dao/Conexao.java
+++ src/main/java/br/edu/ifpi/dao/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:postgresql://db.ijhnuifhmrhpcwvnlezj.supabase.co:5432/postgres?sslmode=require";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "ifpi_bd2023";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
