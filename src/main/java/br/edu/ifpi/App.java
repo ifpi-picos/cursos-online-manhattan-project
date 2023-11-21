0245dec7eda2cfc88dca8abfd98209727e1849d1
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-21 00:57:08 -0300

    problemas com carregamento da cena

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -1,11 +1,12 @@
 package br.edu.ifpi;
 
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -17,8 +18,17 @@ public class App extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        Scene scene = new Scene(root);
+
+        // Criar um layout simples usando um StackPane
+        StackPane root = new StackPane();
+
+        // Configurar a cena
+        Scene scene = new Scene(root, 600, 400);
+        URL resourceUrl = getClass().getResource("/fxml/login.fxml");
+        System.out.println("Resource URL: " + resourceUrl);
+
+        // Parent root = FXMLLoader.load(resourceUrl);
+        // Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
