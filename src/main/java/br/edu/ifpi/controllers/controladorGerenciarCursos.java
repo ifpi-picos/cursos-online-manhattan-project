16bf7d4788fca395c02fc43e31cf6a41944f26ed
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-21 15:00:09 -0300

    Ajeitando exibição de cursos na tela gerenciar cursos

--- src/main/java/br/edu/ifpi/controllers/controladorGerenciarCursos.java
+++ src/main/java/br/edu/ifpi/controllers/controladorGerenciarCursos.java
@@ -1,14 +1,25 @@
 package br.edu.ifpi.controllers;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import br.edu.ifpi.sistema;
+import br.edu.ifpi.dao.Conexao;
+import br.edu.ifpi.dao.CursoDao;
+import br.edu.ifpi.entities.Curso;
 
 
 public class controladorGerenciarCursos implements Initializable {
@@ -32,16 +43,16 @@ public class controladorGerenciarCursos implements Initializable {
     private Button btnSair;
 
     @FXML
-    private TableColumn<?, ?> colunaCargaHoraria;
+    private TableColumn<Curso, Integer> colunaCargaHoraria;
 
     @FXML
-    private TableColumn<?, ?> colunaNome;
+    private TableColumn<Curso, String> colunaNome;
 
     @FXML
-    private TableColumn<?, ?> colunaProfessor;
+    private TableColumn<Curso, String> colunaProfessor;
 
     @FXML
-    private TableView<?> tabelaCursos;
+    private TableView<Curso> tabelaCursos;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -51,6 +62,44 @@ public class controladorGerenciarCursos implements Initializable {
         btnSair.setOnAction(event -> sistema.trocarCena("/fxml/login.fxml", btnSair));
 
         btnAdicionar.setOnAction(event -> sistema.trocarCena("/fxml/cadastroCurso.fxml", btnAdicionar));
+
+        Connection conexao = null;
+        try {
+            conexao = Conexao.getConnection();
+
+            // Chama o construtor de CursoDao passando a conexão como argumento
+            CursoDao BDCurso = new CursoDao(conexao);
+            // Obter a lista de cursos do banco de dados
+            List<Curso> cursosDoBanco = BDCurso.consultarTodos();
+            // Criar uma ObservableList a partir da lista de cursos
+            ObservableList<Curso> observableCursos = FXCollections.observableArrayList(cursosDoBanco);
+
+            // Configurar o TableView e as colunas
+            colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+            colunaCargaHoraria.setCellValueFactory(new PropertyValueFactory<>("cargaHoraria"));
+            colunaProfessor.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getProfessor().getNome()));
+            
+
+            // Configurar a TableView para exibir a lista de cursos
+            tabelaCursos.setItems(observableCursos);
+
+            // Configurar seleção única
+            tabelaCursos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Certifique-se de fechar a conexão no bloco finally
+            if (conexao != null) {
+                try {
+                    conexao.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
 
 }
