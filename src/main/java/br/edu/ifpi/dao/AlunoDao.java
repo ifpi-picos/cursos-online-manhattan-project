0bd7ad8369e4caa23972a1e2876d183419716586
Matheus da Silva <contagodzillajapan@gmail.com>
2023-11-20 21:45:25 -0300

    Construindo método de cadastrar

--- src/main/java/br/edu/ifpi/dao/AlunoDao.java
+++ src/main/java/br/edu/ifpi/dao/AlunoDao.java
@@ -1,5 +1,6 @@
 package br.edu.ifpi.dao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import br.edu.ifpi.entities.Aluno;
 
@@ -10,5 +11,19 @@ public class AlunoDao implements Dao<Aluno>{
         this.connection = connection;
     }
 
-    
+    @Override
+    public int cadastrar(Aluno aluno) {
+        String sql = "INSERT INTO alunos (nome, email) VALUES (?, ?)";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, aluno.getNome());
+            stmt.setString(2, aluno.getEmail());
+            return stmt.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir aluno no banco de dados: " + e.getMessage());
+        } finally {
+            connection.close();
+        }
+    }
 }
