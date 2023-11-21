4b89d9518507cab99b1ccdd9eeec8d69ab9cfbf0
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-21 02:01:49 -0300

    preparando função de cadastro

--- src/main/java/br/edu/ifpi/controllers/controladorCadastro.java
+++ src/main/java/br/edu/ifpi/controllers/controladorCadastro.java
@@ -1,12 +1,24 @@
 package br.edu.ifpi.controllers;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import br.edu.ifpi.sistema;
+import br.edu.ifpi.dao.AlunoDao;
+import br.edu.ifpi.dao.Conexao;
+import br.edu.ifpi.dao.ProfessorDao;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
-public class controladorCadastro {
+import br.edu.ifpi.sistema;
+
+public class controladorCadastro implements Initializable {
 
     @FXML
     private Button btnCadastrar;
@@ -29,4 +41,36 @@ public class controladorCadastro {
     @FXML
     private RadioButton radioProfessor;
 
+    AlunoDao BDAluno;
+    ProfessorDao BDProfessor;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnVoltar.setOnAction(event -> sistema.trocarCena("/fxml/login.fxml", btnCadastrar));
+
+        btnCadastrar.setOnAction(event -> gerarCadastro());
+    }
+
+
+    public void gerarCadastro(){
+        Connection connection = null;
+        try {
+            connection = Conexao.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        BDAluno = new AlunoDao(connection);
+        BDProfessor = new ProfessorDao(connection);
+
+        String nome = inputNome.getText();
+        String email = inputEmail.getText();
+
+        if (sistema.validarEmail(email)){
+            System.out.println("Email válido");
+        }else{
+            
+        }
+
+    }
 }
