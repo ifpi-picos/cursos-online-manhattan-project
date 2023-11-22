1902ed1a8f83ca337d524460504f6a9fdf9d3d2c
Gustavo Figueredo <gustavofigueredo86@gmail.com>
2023-11-21 21:17:04 -0300

    Mudando a dinamica da classe AlunoCurso

--- src/main/java/br/edu/ifpi/entities/AlunoCurso.java
+++ src/main/java/br/edu/ifpi/entities/AlunoCurso.java
@@ -1,64 +1,38 @@
 package br.edu.ifpi.entities;
 
 public class AlunoCurso {
-    private int id;
+    private String id;
     private Aluno aluno;
     private Curso curso;
-    private Double[] nota;
 
-    // Construtores
     public AlunoCurso(Aluno aluno, Curso curso) {
         this.aluno = aluno;
         this.curso = curso;
-        this.nota = new Double[3];
-    }
-    
-    public AlunoCurso(int id, Aluno aluno, Curso curso) {
-        this.id = id;
-        this.aluno = aluno;
-        this.curso = curso;
-        this.nota = new Double[3];
-    }
-    
-    // Getters e Setters
-    public int getId() {
-        return id;
+        this.id = aluno.getId() + "-" + curso.getId();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getId() {
+        return id;
     }
 
     public Aluno getAluno() {
         return aluno;
     }
 
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
     public Curso getCurso() {
         return curso;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
     }
 
     public void setCurso(Curso curso) {
         this.curso = curso;
     }
 
-    public Double[] getNota() {
-        return nota;
-    }
-
-    public void setNota(Double[] nota) {
-        this.nota = nota;
-    }
-    
-    public Double getNota(int i) {
-        return nota[i];
-    }
-    public void setNota(Double nota, int i) {
-        this.nota[i] = nota;
-    }
-    
 }
-
