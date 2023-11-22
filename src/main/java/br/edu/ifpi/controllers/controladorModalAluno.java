1958d1b6f9cd0705434d5d706bd68a30994b0ac5
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2023-11-22 15:43:36 -0300

    controladorModalALuno criado

---
+++ src/main/java/br/edu/ifpi/controllers/controladorModalAluno.java
@@ -0,0 +1,28 @@
+package br.edu.ifpi.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class controladorModalAluno {
+
+    @FXML
+    private TableColumn<?, ?> media;
+
+    @FXML
+    private AnchorPane nodalBackground;
+
+    @FXML
+    private TableColumn<?, ?> nota1;
+
+    @FXML
+    private TableColumn<?, ?> nota2;
+
+    @FXML
+    private TableColumn<?, ?> nota3;
+
+    @FXML
+    private TableView<?> tabelaNotas;
+
+}
