499824ae5f2ee9a052ab00defa7609716f1cc7f6
Matheus da Silva <contagodzillajapan@gmail.com>
2023-12-03 03:35:25 -0300

    Desenvolvendo funcionamento da tabela

--- src/main/java/br/edu/ifpi/controllers/ControladorMeusCursosAluno.java
+++ src/main/java/br/edu/ifpi/controllers/ControladorMeusCursosAluno.java
@@ -1,19 +1,29 @@
 package br.edu.ifpi.controllers;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import br.edu.ifpi.SessaoController;
 import br.edu.ifpi.SessaoDao;
+import br.edu.ifpi.SessaoUsuario;
 import br.edu.ifpi.Sistema;
+import br.edu.ifpi.dao.AlunoCursoDao;
+import br.edu.ifpi.dao.AlunoDao;
+import br.edu.ifpi.dao.Conexao;
+import br.edu.ifpi.entities.Aluno;
 import br.edu.ifpi.entities.AlunoCurso;
 import br.edu.ifpi.enums.StatusAlunoCurso;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ControladorMeusCursosAluno implements Initializable,SessaoController {
 
@@ -79,7 +89,16 @@ public class ControladorMeusCursosAluno implements Initializable,SessaoControlle
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        carregarTabela();
 
+        colNome.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCurso().getNome()));
+        colProfessor.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCurso().getProfessor().getNome()));
+        colNota1.setCellValueFactory(new PropertyValueFactory<>("nota1"));
+        colNota2.setCellValueFactory(new PropertyValueFactory<>("nota2"));
+        colNota3.setCellValueFactory(new PropertyValueFactory<>("nota3"));
+        ColMedia.setCellValueFactory(new PropertyValueFactory<>("media"));
+        colProfessor.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCurso().getProfessor().getNome()));
+        statusMatricula.setCellValueFactory(new PropertyValueFactory<>("statusAlunoCurso"));
 
         btnCursos.setOnAction(event -> Sistema.trocarCena("/fxml/telasAluno/cursosAluno.fxml", btnCursos, sessaoDao));
         btnHome.setOnAction(event -> Sistema.trocarCena("/fxml/telasAluno/telaInicialAluno.fxml", btnCursos, sessaoDao));
@@ -88,5 +107,23 @@ public class ControladorMeusCursosAluno implements Initializable,SessaoControlle
         btnSair.setOnAction(event -> Sistema.trocarCena("/fxml/login.fxml", btnSair, sessaoDao));
     }
 
+    // Função p/ carregar a tabela de cursos do aluno
+    public void carregarTabela(){
+        try {
+            AlunoCursoDao alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
+            AlunoDao alunoDao = new AlunoDao(Conexao.getConnection());
 
+            Aluno aluno = alunoDao.buscarPorNomeEEmail(SessaoUsuario.getNomeUsuario(), SessaoUsuario.getEmailUsuario());
+            
+            List<AlunoCurso> cursos = alunoCursoDao.consultarCursosMatriculados(aluno.getId());
+            ObservableList<AlunoCurso> cursosObservable = FXCollections.observableArrayList(cursos);
+
+            tabelaCursos.setItems(cursosObservable);
+            // List<AlunoCurso> cursos = sessaoDao.getAluno().getCursos();
+            // tabelaCursos.getItems().setAll(cursos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Sistema.exibirPopupErro("Erro ao se conectar ao Banco de dados");
+        }
+    }
 }
