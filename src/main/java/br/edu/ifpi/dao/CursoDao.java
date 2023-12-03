71ebb42f8e05d378774439f1d8bef378eec28ea5
Matheus da Silva <contagodzillajapan@gmail.com>
2023-12-03 01:48:02 -0300

    Método lista dos cursos que o aluno tá matriculado

--- src/main/java/br/edu/ifpi/dao/CursoDao.java
+++ src/main/java/br/edu/ifpi/dao/CursoDao.java
@@ -127,4 +127,39 @@ public class CursoDao implements Dao<Curso>{
         }
         return cursos;
     }
+
+    // Método que retorna uma lista dos cursos que o aluno está matriculado
+    public List<Curso> consultarCursosMatriculados(int idAluno) {
+        List<Curso> cursos = new ArrayList<>();
+    
+        String sql = "SELECT c.id, c.nome, c.carga_horaria, c.id_professor, c.status, p.nome as nome_professor, p.email "
+                + "FROM cursos c "
+                + "JOIN professores p ON c.id_professor = p.id "
+                + "JOIN aluno_curso ac ON c.id = ac.id_curso "
+                + "WHERE ac.id_aluno = ?";
+    
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, idAluno);
+            ResultSet rs = stmt.executeQuery();
+    
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                int cargaHoraria = rs.getInt("carga_horaria");
+                int idProfessor = rs.getInt("id_professor");
+                StatusCurso status = StatusCurso.valueOf(rs.getString("status"));
+                String nomeProfessor = rs.getString("nome_professor");
+                String emailProfessor = rs.getString("email");
+    
+                Professor professor = new Professor(idProfessor, nomeProfessor, emailProfessor);
+                Curso curso = new Curso(id, nome, cargaHoraria, professor, status);
+                cursos.add(curso);
+            }
+    
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao consultar cursos no banco de dados: " + e.getMessage());
+        }
+        return cursos;
+    }
 }
