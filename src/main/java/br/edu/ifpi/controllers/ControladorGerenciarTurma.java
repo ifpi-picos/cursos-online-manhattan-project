202d73fdfbd00ce383918a6e83ed5adfd165fef5
Matheus da Silva <contagodzillajapan@gmail.com>
2023-12-03 22:25:23 -0300

    Funcionamento do cadastro de notas

--- src/main/java/br/edu/ifpi/controllers/ControladorGerenciarTurma.java
+++ src/main/java/br/edu/ifpi/controllers/ControladorGerenciarTurma.java
@@ -103,6 +103,8 @@ public class ControladorGerenciarTurma implements Initializable {
         });
 
         btnNota1.setOnAction(event -> tornarVisivelInputNota1());
+        btnNota2.setOnAction(event -> tornarVisivelInputNota2());
+        btnNota3.setOnAction(event -> tornarVisivelInputNota3());
         btnCadastrar.setOnAction(event -> cadastrarNotas());
         
         btnHome.setOnAction(event -> Sistema.trocarCena("/fxml/telasProfessor/telaInicialProf.fxml",btnHome));
@@ -129,28 +131,72 @@ public class ControladorGerenciarTurma implements Initializable {
         AlunoCurso alunoCursoSelecionado = tabelaTurma.getSelectionModel().getSelectedItem();
 
         // Verificação sobre qual campo tem dados
-
-        if (alunoCursoSelecionado != null) {
-            // Obtenha as notas digitadas nos campos de texto
-            double nota1 = Double.parseDouble(inputNota1.getText());
-            double nota2 = Double.parseDouble(inputNota2.getText());
-            double nota3 = Double.parseDouble(inputNota3.getText());
-            
-
+            if (alunoCursoSelecionado != null) {
             AlunoCursoDao alunoCursoDao;
-            try {
-                alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
-                alunoCursoDao.cadastrarNotas(alunoCursoSelecionado.getAluno().getId(), curso.getId(), nota1, nota2, nota3);
-            } catch (SQLException e) {
-                
-                e.printStackTrace();
+
+            Double nota1 = inputNota1.getText().isEmpty() ? null : Double.parseDouble(inputNota1.getText());
+            Double nota2 = inputNota2.getText().isEmpty() ? null : Double.parseDouble(inputNota2.getText());
+            Double nota3 = inputNota3.getText().isEmpty() ? null : Double.parseDouble(inputNota3.getText());
+
+
+            // Antes de cadastrar as notas, verifique se os dados estão corretos
+            if (nota1 != null && (nota1 < 0 || nota1 > 10)) {
+                Sistema.exibirPopupErro("Espaço vazio ou nota inválida");
+                return;
+            } else if (nota2 != null && (nota2 < 0 || nota2 > 10)) {
+                Sistema.exibirPopupErro("A nota 2 deve estar entre 0 e 10.");
+                return;
+            } else if (nota3 != null && (nota3 < 0 || nota3 > 10)) {
+                Sistema.exibirPopupErro("A nota 3 deve estar entre 0 e 10.");
+                return;
             }
+
+            // Se tá tudo certo, cadastre as notas individualmente
+            // Cadastrar a nota 1
+            if (nota1 != null) {
+                try {
+                    alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
+                    alunoCursoDao.inserirPrimeiraNota(alunoCursoSelecionado.getAluno().getId(), curso.getId(), nota1);
+                    inputNota1.clear();
+                } catch (SQLException e) {
+                    
+                    e.printStackTrace();
+                }
+            } else if (nota2 != null) {
+                try {
+                    alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
+                    alunoCursoDao.inserirSegundaNota(alunoCursoSelecionado.getAluno().getId(), curso.getId(), nota2);
+                    inputNota2.clear();
+                } catch (SQLException e) {
+                    
+                    e.printStackTrace();
+                }
+            } else if (nota3 != null) {
+                try {
+                    alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
+                    alunoCursoDao.inserirTerceiraNota(alunoCursoSelecionado.getAluno().getId(), curso.getId(), nota3);
+                    inputNota3.clear();
+                } catch (SQLException e) {
+                    
+                    e.printStackTrace();
+                }
+            }
+
+            // try {
+            //     alunoCursoDao = new AlunoCursoDao(Conexao.getConnection());
+            //     alunoCursoDao.cadastrarNotas(alunoCursoSelecionado.getAluno().getId(), curso.getId(), nota1, nota2, nota3);
+            // } catch (SQLException e) {
+                
+            //     e.printStackTrace();
+            // }
 
             // Atualize a tabela após a inserção da nota
             carregarDadosTabela();
 
             // Limpe os campos de entrada após a inserção
-            inputNota3.clear();
+            // inputNota1.clear();
+            // inputNota2.clear();
+            // inputNota3.clear();
 
             Sistema.exibirPopupSucesso("Notas cadastradas com sucesso!");
         } else {
@@ -167,6 +213,42 @@ public class ControladorGerenciarTurma implements Initializable {
             // Senão, torne-o visível e habilitado
             inputNota1.setVisible(true);
             inputNota1.setDisable(false);
+        }
+        
+        // Verifica se o botão cadastrar está desabilitado e o torna visível e habilitado
+        if (btnCadastrar.isDisabled()) {
+            btnCadastrar.setVisible(true);
+            btnCadastrar.setDisable(false);
+        }
+    }
+
+    public void tornarVisivelInputNota2() {
+        if (inputNota2.isVisible() && !inputNota2.isDisabled()) {
+            // Se o campo está visível e habilitado, torne-o invisível e desabilitado
+            inputNota2.setVisible(false);
+            inputNota2.setDisable(true);
+        } else {
+            // Senão, torne-o visível e habilitado
+            inputNota2.setVisible(true);
+            inputNota2.setDisable(false);
+        }
+        
+        // Verifica se o botão cadastrar está desabilitado e o torna visível e habilitado
+        if (btnCadastrar.isDisabled()) {
+            btnCadastrar.setVisible(true);
+            btnCadastrar.setDisable(false);
+        }
+    }
+
+    public void tornarVisivelInputNota3() {
+        if (inputNota3.isVisible() && !inputNota3.isDisabled()) {
+            // Se o campo está visível e habilitado, torne-o invisível e desabilitado
+            inputNota3.setVisible(false);
+            inputNota3.setDisable(true);
+        } else {
+            // Senão, torne-o visível e habilitado
+            inputNota3.setVisible(true);
+            inputNota3.setDisable(false);
         }
         
         // Verifica se o botão cadastrar está desabilitado e o torna visível e habilitado
